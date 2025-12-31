384d75397bf9c6174821bbdfd3614a58d47efc54
kamaralam1984 <kamaralam137@gmail.com>
2026-01-01 01:15:31 +0530

    Add Google Ads tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -108,6 +108,24 @@ export default function RootLayout({
           crossOrigin="anonymous"
         />
         
+        {/* Google Analytics (Tag Manager) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AM-1765454983"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AM-1765454983');
+            `,
+          }}
+        />
+        
         <ThemeProvider>
         <AdStatusProvider>
         <LanguageProvider>
